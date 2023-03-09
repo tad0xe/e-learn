b828c763ef266250aad50349d183f702e5fefecc
Tolu <toluarejibadey@gmail.com>
2023-03-09 02:41:39 +0100

    m

--- app.js
+++ app.js
@@ -1,104 +1,112 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
-var expressValidator = require('express-validator');
-var flash = require('connect-flash');
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local'),Strategy;
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/elear');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var expressValidator = require("express-validator");
+var flash = require("connect-flash");
+var session = require("express-session");
+var passport = require("passport");
+var LocalStrategy = require("passport-local"),
+  Strategy;
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1/elear");
 var db = mongoose.connection;
-async = require('async');
+async = require("async");
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var classes = require('./routes/classes');
-var students = require('./routes/students');
-var instructors = require('./routes/instructors');
+var routes = require("./routes/index");
+var users = require("./routes/users");
+var classes = require("./routes/classes");
+var students = require("./routes/students");
+var instructors = require("./routes/instructors");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout:'layout'}));
-app.set('view engine', 'handlebars');
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs({ defaultLayout: "layout" }));
+app.set("view engine", "handlebars");
+app.set("trust proxy", 1);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Express Session
-app.use(session({
-    secret: 'secret',
+app.use(
+  session({
+    cookie: {
+      secure: true,
+      maxAge: 60000
+    },
+    store: new RedisStore(),
+    secret: "secret",
     saveUninitialized: true,
     resave: true
-}));
+  })
+);
 
 // Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 // Express Validator
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
 
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
     }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
+  })
+);
 
 // Connect-Flash
 app.use(flash());
 
 // Makes the user object global in all views
-app.get('*', function(req, res, next) {
+app.get("*", function(req, res, next) {
   // put user into res.locals for easy access from templates
   res.locals.user = req.user || null;
-  if(req.user){
+  if (req.user) {
     res.locals.type = req.user.type;
   }
   next();
 });
 
-
 // Global Vars
-app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   next();
 });
 
-
-app.use('/', routes);
-app.use('/users', users);
-app.use('/classes', classes);
-app.use('/students', students);
-app.use('/instructors', instructors);
+app.use("/", routes);
+app.use("/users", users);
+app.use("/classes", classes);
+app.use("/students", students);
+app.use("/instructors", instructors);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -107,10 +115,10 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get("env") === "development") {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
       message: err.message,
       error: err
     });
@@ -121,11 +129,10 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: {}
   });
 });
-
 
 module.exports = app;
